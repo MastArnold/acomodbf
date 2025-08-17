57f6df15e89403ee54e57d551722a1cf4a53c6e9
Lossi Arnold Jonathan <102259623+MastArnold@users.noreply.github.com>
2025-08-17 01:10:30 +0000

    add: website controller

---
+++ resources/views/website/index.blade.php
@@ -0,0 +1,27 @@
+<section class="banners">
+
+</section>
+
+<section class="services">
+    
+</section>
+
+<section class="videos">
+    
+</section>
+
+<section class="gallery">
+    
+</section>
+
+<section class="team">
+    
+</section>
+
+<section class="partners">
+    
+</section>
+
+<section class="contact">
+    
+</section>
